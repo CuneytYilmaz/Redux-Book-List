81a623d9c3cf06ee23c90f35bf460047b8e50e50
Cüneyt Yılmaz <cuneyilmaz@gmail.com>
2017-07-26 02:38:48 +0300

    Book Reducer created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,8 @@
-// ./src/reducers/index.js
-
-import { combineReducers } from 'redux';
+import BooksReducer from "./reducer_books";
+import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
-  foo: (state = {}) => state
+  books: BooksReducer
 });
 
 export default rootReducer;
